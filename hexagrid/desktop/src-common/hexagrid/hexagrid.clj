1ae300d1851a3334077366f3465659855a0ffe21
Niv Pagir <nivpgir@gmail.com>
2017-07-07 16:24:13 +0300

    drawing hexas, angle works, removed :flat? - x, y of hexa not workin

--- hexagrid/desktop/src-common/hexagrid/hexagrid.clj
+++ hexagrid/desktop/src-common/hexagrid/hexagrid.clj
@@ -1,11 +1,17 @@
 (ns hexagrid.hexagrid)
 
-;; (defn make-hex [size flat?]
-;;   (cond (flat?)
-;;         (:p1 (- size)
-;;          :p2
-;;          :p3
-;;          :p4 size
-;;          :p5
-;;          :p6
-;;              )
+
+
+(defn hexa-corners [x y size & {:keys [angle-offset] :or {angle-offset 0}}]
+  (loop [i 0 coords []]
+    ;; (println "coords:" coords "i:" i)
+    (if (= i 6) coords
+        (recur (inc i) (into [] (concat coords (->> i
+                                           (* 60)
+                                           (+ angle-offset)
+                                           (Math/toRadians)
+                                           ((fn  [ang]
+                                              ((juxt (fn [rad] (Math/cos rad))
+                                                     (fn [rad] (Math/sin rad))) ang)))
+                                           (map (fn [dir] (* size dir))))
+                               ))))))
